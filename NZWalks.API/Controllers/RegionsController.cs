5d630be40ffd24e7ca51780f96c90af8ceb2571c
affanyounus <affanyounus91@gmail.com>
2023-09-21 18:31:14 +0500

    Add entity framework for sql server, apply two migrations

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Regions()
+        {
+            var regions = new List<Region>
+            {
+                Capacity = 0
+            };
+
+            regions.Add(new Region
+            {
+                Id = Guid.NewGuid(),
+                Name = "Auckland Region",
+                Code = "AKL",
+                RegionImageUrl = "https://www.pexels.com/photo/young-woman-sitting-in-an-armchair-in-a-modern-room-18238186/"
+            });
+
+            regions.Add(new Region
+            {
+                Id = Guid.NewGuid(),
+                Name = "Maryland",
+                Code = "MRY",
+                RegionImageUrl = "https://www.pexels.com/photo/feeling-series-18417020/"
+            });
+
+            return Ok(regions);
+        }
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<NZWalksDbContext>(options => 
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("NZWalksConnectionString")
+        )
+    );
 
 var app = builder.Build();
 
